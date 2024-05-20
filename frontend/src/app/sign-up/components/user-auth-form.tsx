0ab858fe6f04839922d0e8e4a7f099dc215dfc12
Jerónimo Mendes <jeronimo.m.mendes@gmail.com>
2024-05-20 23:14:33 +0100

    #9 - init next js and add basic sign up page

---
+++ frontend/src/app/sign-up/components/user-auth-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema: any = z.object({
+    username: z.string().min(5, {
+        message: "Username must be at least 5 characters long.",
+    }),
+    email: z.string().email({
+        message: "Please enter a valid email address.",
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters long.",
+    }),
+}).extend({
+    passwordConfirmation: z.string().refine((data) => data === formSchema.password, {
+        message: "Passwords do not match.",
+    }),
+});
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            email: "",
+        },
+    })
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        setIsLoading(true);
+
+        console.log(values)
+        setTimeout(() => {
+        setIsLoading(false);
+        }, 3000);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-2">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="JoeDoe" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="joedoe@example.com" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                We&apos;ll never share your email with anyone else.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="supersecret123" type="password" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Must be at least 8 characters long.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="passwordConfirmation"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Confirm password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="supersecret123" type="password" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Must match the password above.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Sign up</Button>
+            </form>
+        </Form>
+    )
+}
