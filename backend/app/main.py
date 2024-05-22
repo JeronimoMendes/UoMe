463e9029e77b5feefd6841baecb503e79810422b
Jerónimo Mendes <jeronimo.m.mendes@gmail.com>
2024-05-23 00:45:56 +0100

    #9 - add cors policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,25 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.auth_router import auth_router
 from app.routers.expense_router import expense_router
 from app.routers.group_router import group_router
 
 app = FastAPI()
+
+origins = [
+    os.getenv("FRONTEND_URL", "http://localhost:3000"),
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, tags=["Auth"])
 app.include_router(group_router, tags=["Group"])
