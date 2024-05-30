9ac3013e9e6e1773d8bc4d6ca884980bc694e86f
Jerónimo Mendes <jeronimo.m.mendes@gmail.com>
2024-05-30 23:19:42 +0100

    #23 - change migration scrip to get db url from env var

--- backend/db/env.py
+++ backend/db/env.py
@@ -5,10 +5,13 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from app.models import *  # noqa
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
